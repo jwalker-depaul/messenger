44b5ed8a379c18e52816a877b82a17b6f58df938
Joseph <jwalker.depaul@gmail.com>
2018-11-03 21:58:43 -0400

    Added clean exiting for the client!

--- client.py
+++ client.py
@@ -1,8 +1,10 @@
 import socket
 import sys
+from threading import Thread
 
 LOCAL_HOST = "127.0.0.1"
 SERVER_PORT = 1337
+BUFF_SIZE = 1024
 
 # Check for input (Need name and port)
 if (len(sys.argv) < 3):
@@ -14,26 +16,45 @@ class Client:
     clientSocket = socket.socket()
     port = ""
     name = ""
+    recvThread = Thread()
+    sendThread = Thread()
     
     def __init__(self):
         self.name = sys.argv[1]
         self.port = sys.argv[2]
         self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Setup UDP Socket
         self.clientSocket.bind((LOCAL_HOST, int(self.port)))
-
-    def run(self):
         self.clientSocket.sendto("PORT=" + self.port, (LOCAL_HOST, SERVER_PORT))
-        while True:
-            message = raw_input("")
-            self.clientSocket.sendto((self.name + ": " + message), ((LOCAL_HOST, SERVER_PORT))
+        
+    def run(self):
+        #Thread for recieve
+        self.recvThread = Thread(target = self.recieveMessages)
+        self.sendThread = Thread(target = self.sendMessages)
+        
+        # Start threads
+        self.recvThread.start()
+        self.sendThread.start()
 
-        break
+        # Join threads
+        self.recvThread.join()
+        self.sendThread.join()
                          
     def sendMessages(self):
-        pass
+        while True:
+            message = raw_input("")
+            if message.lower() == "exit":
+                # Tell server you're leaving
+                self.clientSocket.sendto(("REMOVE_CLIENT=" + self.name), (LOCAL_HOST, SERVER_PORT))
+                break
+            else:    
+                self.clientSocket.sendto((self.name + ": " + message), (LOCAL_HOST, SERVER_PORT))
 
     def recieveMessages(self):
-        pass
+        while True:
+            data, addr = self.clientSocket.recvfrom(BUFF_SIZE)
+            if (data == "CLIENT_EXIT"):
+                break
+            print data
 
 if __name__ == '__main__':
     client = Client()
--- server.py
+++ server.py
@@ -19,13 +19,31 @@ class Server:
             # Recieve message
             data, addr = self.serverSocket.recvfrom(BUFF_SIZE)
             if ("PORT=" in data):
-                # New client connected, store into collection
-                clientPort = data[5:]
-                self.clients[clientPort] = addr
-                print self.clients
-                
-            print(data)
+                # New client connected
+                self.handleClient(data, addr)
+            elif ("REMOVE_CLIENT=" in data):
+                self.removeClient(data, addr)
+            else:
+                self.handleMessage(data, addr)
 
+    def handleClient(self, data, addr):
+        clientPort = int(data[5:])
+        self.clients[clientPort] = addr
+        print self.clients
+
+    def removeClient(self, data, addr):
+        name = data[14:]
+        #Tell client to exit
+        self.serverSocket.sendto("CLIENT_EXIT", addr)
+        self.handleMessage(name + " has left", addr) 
+        del self.clients[addr[1]] # remove from dictionary
+        print self.clients
+
+    def handleMessage(self, data, addr):
+        for i in self.clients:
+            if i != addr[1]:
+                self.serverSocket.sendto(data, self.clients[i])
+            
 if __name__ == '__main__':
     server = Server()
     server.run()
