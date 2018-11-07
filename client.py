35fea58cd4b94bd714d33f8ebd9c90e52052340b
jwalker-depaul <jwalker.depaul@gmail.com>
2018-11-07 09:01:47 -0500

    Minor updates to documentation and variable naming conventions

--- client.py
+++ client.py
@@ -12,23 +12,31 @@ if (len(sys.argv) < 3):
     sys.exit(0)
 
 class Client:
-    # Socket setup
+    # Client info
     clientSocket = socket.socket()
     port = ""
     name = ""
+
+    # Threads
     recvThread = Thread()
     sendThread = Thread()
     
     def __init__(self):
+        # Grab info from cla
         self.name = sys.argv[1]
         self.port = sys.argv[2]
-        self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Setup UDP Socket
+
+        # Setup UDP socket
+        self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.clientSocket.bind((LOCAL_HOST, int(self.port)))
+
+        # Notify server of client join
         self.clientSocket.sendto("PORT=" + self.port, (LOCAL_HOST, SERVER_PORT))
+        self.clientSocket.sendto(self.name + " has come to chat!", (LOCAL_HOST, SERVER_PORT))
         
     def run(self):
-        #Thread for recieve
-        self.recvThread = Thread(target = self.recieveMessages)
+        # Setup threads
+        self.recvThread = Thread(target = self.receiveMessages)
         self.sendThread = Thread(target = self.sendMessages)
         
         # Start threads
@@ -41,19 +49,25 @@ class Client:
                          
     def sendMessages(self):
         while True:
+            # Read in message from command line
             message = raw_input("")
             if message.lower() == "exit":
                 # Tell server you're leaving
                 self.clientSocket.sendto(("REMOVE_CLIENT=" + self.name), (LOCAL_HOST, SERVER_PORT))
                 break
             else:    
+                # Just send it bro
                 self.clientSocket.sendto((self.name + ": " + message), (LOCAL_HOST, SERVER_PORT))
 
-    def recieveMessages(self):
+    def receiveMessages(self):
         while True:
+            # Receive from server
             data, addr = self.clientSocket.recvfrom(BUFF_SIZE)
             if (data == "CLIENT_EXIT"):
+                # Kill the thread
                 break
+
+            # Print messages from other users
             print data
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -7,39 +7,47 @@ BUFF_SIZE = 1024
 
 class Server:
     serverSocket = socket.socket()
-    port = ""
     clients = {}
+    ip = ""
+    port = ""
 
     def __init__(self):
+        # Setup UDP socket
         self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Setup UDP socket
         self.serverSocket.bind((LOCAL_HOST, PORT))
 
     def run(self):
         while True:
-            # Recieve message
+            # Recieve messages
             data, addr = self.serverSocket.recvfrom(BUFF_SIZE)
             if ("PORT=" in data):
                 # New client connected
-                self.handleClient(data, addr)
+                self.addClient(data, addr)
             elif ("REMOVE_CLIENT=" in data):
+                # Client is leaving
                 self.removeClient(data, addr)
             else:
+                # Regular message
                 self.handleMessage(data, addr)
 
-    def handleClient(self, data, addr):
+    def addClient(self, data, addr):
+        # Add client to collection of clients
         clientPort = int(data[5:])
         self.clients[clientPort] = addr
         print self.clients
 
     def removeClient(self, data, addr):
+        # Tell client to exit
         name = data[14:]
-        #Tell client to exit
         self.serverSocket.sendto("CLIENT_EXIT", addr)
+
+        # Remove client from collection and report
         self.handleMessage(name + " has left", addr) 
         del self.clients[addr[1]] # remove from dictionary
         print self.clients
 
     def handleMessage(self, data, addr):
+        # Send message to everyone except the original sender
         for i in self.clients:
             if i != addr[1]:
                 self.serverSocket.sendto(data, self.clients[i])
